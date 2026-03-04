9144c449f180f03552bb3c1c6cf13d76d2877048
GuiBlanco0 <guilhermeblancoribeiro@gmail.com>
2026-03-03 21:57:05 -0300

    Changes to 4 ports

--- Logical Ports.py
+++ Logical Ports.py
@@ -1,10 +1,16 @@
 A = 0
 B = 0
-print("A B | X")
-for A in range (2):
-    for B in range(2):
-        X =0
-        if A and B:
-            X = 1
-        print(A,B,"|",X)
+C = 0
+D = 0
+
+print("A B C D | X")
+for A in range(2):
+  for B in range(2):
+    for C in range(2):
+     for D in range(2):
+         X = 0
+         if A and B:
+          X = 1
+ 
+    print(A,B,C,D,'|', X)
             
