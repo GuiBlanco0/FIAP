5d64ca1515b4beb02b0fd759e25ab5aa687889e4
GuiBlanco0 <guilhermeblancoribeiro@gmail.com>
2026-03-03 22:31:51 -0300

    Challenge Solved

--- Logical Ports.py
+++ Logical Ports.py
@@ -1,11 +1,9 @@
-#A security system opens the door only when the card
-#and the fingerprint matches, or if the emergency key was used (C = 1)
-#Logical expression - A*B+C = X
-card = 0
-fgp = 0
-key = 1
-
-if card * fgp + key:
-    print("OPEN DOOR")
+#Exercise challenge
+ocSensor = 1
+lumSensor = 0
+winSensor = 0
+nMode = 0
+if (ocSensor and not(lumSensor) and not(winSensor)) or nMode:
+    print("Lights ON")
 else:
-    print("CLOSED DOOR")
+    print("Lights OFF")
