422b9d7a61e882b715adb16451bf4459c69c1c48
GuiBlanco0 <guilhermeblancoribeiro@gmail.com>
2026-03-04 19:37:44 -0300

    Learning to use files

--- File and JSON manipulation/File and JSON manipulation.py
+++ File and JSON manipulation/File and JSON manipulation.py
@@ -1,4 +1,4 @@
-with open("teste.txt", "w") as file:
-    file.write("Line One")
-    file.write("\nLine Two")
-    file.write("\nLine Three")
+open("teste.txt", "w").close()  # limpa o arquivo
+with open("teste.txt", "a") as arquivo:
+    for i in range(1, 10):
+        arquivo.write(f"Linha {i}\n")
