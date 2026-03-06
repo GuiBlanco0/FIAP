4e02878292cdffb709c956739dc32c0c9cf2bcc6
GuiBlanco0 <guilhermeblancoribeiro@gmail.com>
2026-03-05 22:22:56 -0300

    Strings

--- Structures.c
+++ Structures.c
@@ -1,11 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
 struct myStructure {
-    int myNum; char myLetter;
+    int myNum; char myLetter; char myString[30];
 };
 int main(){
     struct myStructure s1;
-    s1.myNum = 13;
-    s1.myLetter = "B";
-    printf("My number: %d\n", s1.myNum);
-    printf("My Letter: %d", s1.myLetter);
+
+    strcpy(s1.myString, "Some text");
+
+
+    printf("My Letter: %s", s1.myString);
     return 0;
 }
