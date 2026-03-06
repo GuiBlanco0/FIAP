7b9b63f299bd7cebc681dc087c3bfe1d015518fe
GuiBlanco0 <guilhermeblancoribeiro@gmail.com>
2026-03-05 22:30:11 -0300

    Changing variables

--- Structures.c
+++ Structures.c
@@ -2,26 +2,23 @@
 #include <string.h>
 
 struct myStructure {
-    int myNum; char myLetter; char myString[30];
+  int myNum;
+  char myLetter;
+  char myString[30];
 };
-int main(){
-    struct myStructure s1;
-    s1.myNum = 15;
-    s1.myLetter = "A";
-    strcpy(s1.myString, "Some text");
 
-    struct myStructure s2;
-    s2 = s1;
+int main() {
+  struct myStructure s1 = {13, 'B', "Some text"};
 
-    printf("%d %c %s", s2.myNum, s2.myLetter, s2.myString)
+  struct myStructure s2;
 
+  s2 = s1;
 
+  s2.myNum = 30;
+  s2.myLetter = 'C';
+  strcpy(s2.myString, "Something else");
 
-
-
-
-    return 0;
+  printf("%d %c %s\n", s1.myNum, s1.myLetter, s1.myString);
+  printf("%d %c %s\n", s2.myNum, s2.myLetter, s2.myString);
+  return 0;
 }
-
-
-
