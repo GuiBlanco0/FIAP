748fb65bac77af9a1bca59594573e2dccb5970fb
Blanco <guilhermeblancoribeiro@gmail.com>
2026-04-16 22:40:21 -0300

    Delete Logical Ports.py

--- Logical Ports.py
+++
@@ -1,9 +0,0 @@
-#Exercise challenge
-ocSensor = 1
-lumSensor = 0
-winSensor = 0
-nMode = 0
-if (ocSensor and not(lumSensor) and not(winSensor)) or nMode:
-    print("Lights ON")
-else:
-    print("Lights OFF")
